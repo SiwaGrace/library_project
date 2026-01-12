a3fa9100c7d061defe4fe317ea16b798f6a6ecf5
SiwaGrace <moneyesenam@gmail.com>
2026-01-12 10:49:39 +0000

    npm run build

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,7 +3,8 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Book Library</title>
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
   </head>
 
   <body class="min-h-screen bg-gray-50 text-gray-800 font-sans">
